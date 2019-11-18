389d04de0532ef9d8614069c0d811557f24fabc1
Miguel Moreno <miguelazomm@gmail.com>
2019-11-18 08:28:50 -0400

    corrigiendo_reverse

--- catalog/urls.py
+++ catalog/urls.py
@@ -12,4 +12,13 @@ urlpatterns = [
     url(r'^authors/', views.AuthorListView.as_view(), name='authors'),
     url(r'^author/(?P<pk>\d+)$', views.AuthorDetailView.as_view(), name='author-detail'),
 
+
+]
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
+urlpatterns += [   
+    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
